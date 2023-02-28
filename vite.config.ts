9939c8b0caeb7d1705e4e3c6707b33f09b9495f4
JianJroh <rhjian@foxmail.com>
2023-02-28 18:07:18 +0800

    perf: build dts by tsc instead of vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react()],
   server: {
     open: '/demo/index.html',
   },
